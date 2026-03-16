0f94f940f88f2f97773fff28a341308d6aa21606
muhmdpp <connect.mhdpp@gmail.com>
2026-03-16 17:56:06 +0400

    bottom-issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -265,7 +265,6 @@ export default function Home() {
                     </>
                 )}
 
-                <div style={{ height: '100px' }} />
             </main>
 
             <BottomNav />
